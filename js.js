79884b53a7565a0f528d70990c78cd551a120d9d
ginpei <ginpei@gmail.com>
2016-05-05 16:12:24 -0700

    rename

--- js.js
+++ js.js
@@ -10,7 +10,7 @@
 
 	var line = d3.svg.line();
 	line.x(d=>x(d.date));
-	line.y(d=>y(d.close));
+	line.y(d=>y(d.value));
 
 	var svg = d3.select('#basic').append('svg');
 	svg.attr('width', width);
@@ -21,7 +21,7 @@
 	var data = getData();
 
 	x.domain(d3.extent(data, d=>d.date));
-	y.domain(d3.extent(data, d=>d.close));
+	y.domain(d3.extent(data, d=>d.value));
 
 	svg.append('path')
 		.datum(data)
@@ -52,7 +52,7 @@
 
 	var line = d3.svg.line();
 	line.x(d=>x(d.date));
-	line.y(d=>y(d.close));
+	line.y(d=>y(d.value));
 
 	var svg = d3.select('#axis').append('svg');
 	svg.attr('width', baseWidth);
@@ -64,7 +64,7 @@
 	var data = getData();
 
 	x.domain(d3.extent(data, d=>d.date));
-	y.domain(d3.extent(data, d=>d.close));
+	y.domain(d3.extent(data, d=>d.value));
 
 	svg.append('g')
 		.attr('class', 'x axis')
@@ -105,67 +105,67 @@ function getSizes() {
 
 function getData() {
 	return [
-		{date:'2007-05-01', close:129.47},
-		{date:'2007-06-01', close:118.4},
-		{date:'2007-07-01', close:121.26},
-		{date:'2007-08-01', close:135},
-		{date:'2007-09-01', close:144.16},
-		{date:'2007-10-01', close:156.34},
-		{date:'2007-11-01', close:187.44},
-		{date:'2007-12-01', close:178.86},
-		{date:'2008-01-01', close:194.84},
-		{date:'2008-02-01', close:133.75},
-		{date:'2008-03-01', close:121.73},
-		{date:'2008-04-01', close:149.53},
-		{date:'2008-05-01', close:180},
-		{date:'2008-06-01', close:186.1},
-		{date:'2008-07-01', close:174.68},
-		{date:'2008-08-01', close:156.66},
-		{date:'2008-09-01', close:166.19},
-		{date:'2008-10-01', close:109.12},
-		{date:'2008-10-01', close:111.04},
-		{date:'2008-11-01', close:92.67},
-		{date:'2008-12-01', close:86.61},
-		{date:'2009-01-01', close:94.2},
-		{date:'2009-02-01', close:89.31},
-		{date:'2009-03-01', close:104.49},
-		{date:'2009-04-01', close:123.9},
-		{date:'2009-05-01', close:135.07},
-		{date:'2009-06-01', close:142.44},
-		{date:'2009-07-01', close:160.1},
-		{date:'2009-08-01', close:167.41},
-		{date:'2009-09-01', close:182.37},
-		{date:'2009-10-01', close:202.48},
-		{date:'2009-11-01', close:204.44},
-		{date:'2009-12-01', close:202.1},
-		{date:'2010-01-01', close:208.07},
-		{date:'2010-02-01', close:201.67},
-		{date:'2010-03-01', close:224.75},
-		{date:'2010-04-01', close:244.59},
-		{date:'2010-05-01', close:248.34},
-		{date:'2010-06-01', close:271.87},
-		{date:'2010-07-01', close:249.9},
-		{date:'2010-08-01', close:247.64},
-		{date:'2010-09-01', close:268.06},
-		{date:'2010-10-01', close:300.14},
-		{date:'2010-11-01', close:316.66},
-		{date:'2010-12-01', close:321.67},
-		{date:'2011-01-01', close:344.42},
-		{date:'2011-02-01', close:356.85},
-		{date:'2011-03-01', close:345.43},
-		{date:'2011-04-01', close:336.13},
-		{date:'2011-05-01', close:340.5},
-		{date:'2011-06-01', close:332.44},
-		{date:'2011-07-01', close:357.77},
-		{date:'2011-08-01', close:376.99},
-		{date:'2011-09-01', close:384.62},
-		{date:'2011-10-01', close:402.19},
-		{date:'2011-11-01', close:385.22},
-		{date:'2011-12-01', close:391.84},
-		{date:'2012-01-01', close:421.39},
-		{date:'2012-02-01', close:502.6},
-		{date:'2012-03-01', close:589.58},
-		{date:'2012-04-01', close:605.23},
-		{date:'2012-05-01', close:582.13},
+		{date:'2007-05-01', value:129.47},
+		{date:'2007-06-01', value:118.4},
+		{date:'2007-07-01', value:121.26},
+		{date:'2007-08-01', value:135},
+		{date:'2007-09-01', value:144.16},
+		{date:'2007-10-01', value:156.34},
+		{date:'2007-11-01', value:187.44},
+		{date:'2007-12-01', value:178.86},
+		{date:'2008-01-01', value:194.84},
+		{date:'2008-02-01', value:133.75},
+		{date:'2008-03-01', value:121.73},
+		{date:'2008-04-01', value:149.53},
+		{date:'2008-05-01', value:180},
+		{date:'2008-06-01', value:186.1},
+		{date:'2008-07-01', value:174.68},
+		{date:'2008-08-01', value:156.66},
+		{date:'2008-09-01', value:166.19},
+		{date:'2008-10-01', value:109.12},
+		{date:'2008-10-01', value:111.04},
+		{date:'2008-11-01', value:92.67},
+		{date:'2008-12-01', value:86.61},
+		{date:'2009-01-01', value:94.2},
+		{date:'2009-02-01', value:89.31},
+		{date:'2009-03-01', value:104.49},
+		{date:'2009-04-01', value:123.9},
+		{date:'2009-05-01', value:135.07},
+		{date:'2009-06-01', value:142.44},
+		{date:'2009-07-01', value:160.1},
+		{date:'2009-08-01', value:167.41},
+		{date:'2009-09-01', value:182.37},
+		{date:'2009-10-01', value:202.48},
+		{date:'2009-11-01', value:204.44},
+		{date:'2009-12-01', value:202.1},
+		{date:'2010-01-01', value:208.07},
+		{date:'2010-02-01', value:201.67},
+		{date:'2010-03-01', value:224.75},
+		{date:'2010-04-01', value:244.59},
+		{date:'2010-05-01', value:248.34},
+		{date:'2010-06-01', value:271.87},
+		{date:'2010-07-01', value:249.9},
+		{date:'2010-08-01', value:247.64},
+		{date:'2010-09-01', value:268.06},
+		{date:'2010-10-01', value:300.14},
+		{date:'2010-11-01', value:316.66},
+		{date:'2010-12-01', value:321.67},
+		{date:'2011-01-01', value:344.42},
+		{date:'2011-02-01', value:356.85},
+		{date:'2011-03-01', value:345.43},
+		{date:'2011-04-01', value:336.13},
+		{date:'2011-05-01', value:340.5},
+		{date:'2011-06-01', value:332.44},
+		{date:'2011-07-01', value:357.77},
+		{date:'2011-08-01', value:376.99},
+		{date:'2011-09-01', value:384.62},
+		{date:'2011-10-01', value:402.19},
+		{date:'2011-11-01', value:385.22},
+		{date:'2011-12-01', value:391.84},
+		{date:'2012-01-01', value:421.39},
+		{date:'2012-02-01', value:502.6},
+		{date:'2012-03-01', value:589.58},
+		{date:'2012-04-01', value:605.23},
+		{date:'2012-05-01', value:582.13},
 	].map(v=>{ v.date=new Date(v.date); return v; });
 }
